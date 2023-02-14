875d30e78d4ceb76611cbad530186e69ae495a50
Francisco Boisier <fboisier@codingdojo>
2023-02-13 22:42:28 -0300

    se termina CRUD completo

--- client/src/components/PersonaForm.jsx
+++ client/src/components/PersonaForm.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
-import axios from 'axios';
-import Swal from 'sweetalert2'
 
 
-const initialValues = {
-    nombre: '',
-    apellido: '',
-    edad: 18
-}
 
 const PersonasErrores = Yup.object().shape({
     nombre: Yup.string()
@@ -26,38 +19,13 @@ const PersonasErrores = Yup.object().shape({
         .positive('No puede ser negativo'),
 });
 
-const PersonaForm = () => {
-    const handleSubmit = async(values, actions) => {
-
-        try {
-            const respuesta = await axios.post(`${process.env.REACT_APP_API_URL}/people`, values);
-            console.log(respuesta);
-            if (respuesta.status === 200){
-                Swal.fire({
-                    icon: 'success',
-                    title: 'GENIAL!!!',
-                    text: `Se ha agregado ${respuesta.data.nombre} perfectamente!`,
-                });
-
-                actions.resetForm(initialValues);
-            }
-        } catch (error) {
-            console.log(error);
-            Swal.fire({
-                icon: 'error',
-                title: 'Ops que mal!!!',
-                text: `Error: ${error?.response?.data?.message || error.message}`,
-            })
-        }
-
-
-        
-    }
+const PersonaForm = ({initialValues, botonTexto, onSubmit}) => {
 
   return (
     <Formik 
+        enableReinitialize={true}
         initialValues={initialValues}
-        onSubmit={handleSubmit}
+        onSubmit={onSubmit}
         validationSchema={PersonasErrores}
     >
         {({ errors, touched, isValid, dirty }) => (
@@ -68,7 +36,7 @@ const PersonaForm = () => {
                 {touched.apellido && errors.apellido && <div className="ms-3 mt-1 text-danger">{errors.apellido}</div>}
                 <Field name="edad" type="number" className="form-control mt-2" placeholder="Edad" />
                 {touched.edad && errors.edad && <div className="ms-3 mt-1 text-danger">{errors.edad}</div>}
-                <button className="btn btn-primary mt-5" disabled={!(isValid && dirty)}>Crear Persona</button>
+                <button className="btn btn-primary mt-5" disabled={!(isValid && dirty)}>{botonTexto} Persona</button>
             </Form>
         )}
     </Formik>
--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layouts/Layout";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+import PersonaDetalle from "../pages/personas/PersonaDetalle";
+import PersonaEditar from "../pages/personas/PersonaEditar";
 import Personas from "../pages/personas/Personas";
 import PersonasAdd from "../pages/personas/PersonasAdd";
 
@@ -22,6 +24,14 @@ export default createBrowserRouter([
             {
                 path:'personas/agregar',
                 element: <PersonasAdd />
+            },
+            {
+                path:'personas/:id',
+                element: <PersonaDetalle />
+            },
+            {
+                path:'personas/:id/editar',
+                element: <PersonaEditar />
             }
         ]
     }
